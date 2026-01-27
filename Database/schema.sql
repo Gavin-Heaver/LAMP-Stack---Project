4917ace2332f97473379eabc2a67667fb6193f6e
Dani <do3occ@gmail.com>
2026-01-27 17:32:32 -0500

    Add MySQL schema for contact manager

---
+++ Database/schema.sql
@@ -0,0 +1,59 @@
+/*
+**  Database Schema for LAMP Stack Personal Contact Manager  **
+**    Created by Daniela Cifuentes Cabrera  -- Group 20      **
+*/
+
+-- Create database if not existing so there's no error if schema is imported more than once  --
+CREATE DATABASE IF NOT EXISTS contact_manager;
+-- Select database so all tables created in it
+USE contact_manager;
+
+
+--  Users Table - stores account info for registered users  --
+CREATE TABLE users
+(
+    -- user ID --
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    -- personal info --
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+
+    -- login info --
+    username VARCHAR(50) NOT NULL UNIQUE,
+    -- password is hashed, length to support hashing algorithms
+    password VARCHAR(255) NOT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+--  Contacts Table - stores unique contacts for users  --
+CREATE TABLE contacts
+(
+    -- Unique contact ID --
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    -- User ID of contact owner --
+    userId INT NOT NULL,
+
+    -- contact info --
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+
+    -- timestamp created --
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    -- Foreign Key Constraint for users, if user deleted all associated contacts deleted --
+    CONSTRAINT fk_contacts_user
+        FOREIGN KEY (userId)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+--  Index for search  --
+--    queries are filtered by userId and lastName / firstName  --
+CREATE INDEX idx_contacts_user_search
+ON contacts (userId, lastName, firstName);
