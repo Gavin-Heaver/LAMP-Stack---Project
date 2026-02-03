d4da76efa7e2656555a766e8cceb600b378984dd
Daniel Rojas <danv22852@gmail.com>
2026-02-03 18:07:57 -0500

    Updated AddContact, Login, Register php files
    Updated the AddContact.php, Login.php, and Register.php to perform. Still need to switch the user to login to the database

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,24 +1,26 @@
-
 <?php
-
 	$inData = getRequestInfo();
 	
 	$id = 0;
 	$firstName = "";
 	$lastName = "";
-
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
-	if( $conn->connect_error )
+	
+	//change the connection to sql database to match updated
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 
+		
+	if( $conn->connect_error ) //check connection if it good
 	{
 		returnWithError( $conn->connect_error );
-	}
-	else
+	} 
+	else // if it is do this 
 	{
+		//need to add code to do the hashing stuff
 		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt->bind_param("ss", $inData["login"], $inData["password"]); 
 		$stmt->execute();
 		$result = $stmt->get_result();
-
+		
+		
 		if( $row = $result->fetch_assoc()  )
 		{
 			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
@@ -32,27 +34,26 @@
 		$conn->close();
 	}
 	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
+	function getRequestInfo() // pulls the info from the json, fills into $inData
+		{
+			return json_decode(file_get_contents('php://input'), true);
+		}
+		
 	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
+		{
+			header('Content-type: application/json');
+			echo $obj;
+		}
+			
 	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+		{
+			$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+			sendResultInfoAsJson( $retValue );
+		}
+			
 	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+		{
+			$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+			sendResultInfoAsJson( $retValue );
+		}
 ?>
